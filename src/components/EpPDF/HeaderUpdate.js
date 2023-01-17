61289f4f147f417e5c8177f4d0452b2386ef3cdf
morshedul-antor <antor.morshedul@gmail.com>
2023-01-17 18:27:00 +0600

    add: header fetching code

--- src/components/EpPDF/HeaderUpdate.js
+++ src/components/EpPDF/HeaderUpdate.js
@@ -1,17 +1,55 @@
-import { useState, useReducer, useContext } from 'react'
+import { useState, useReducer, useContext, useEffect } from 'react'
 import { Auth } from '../../allContext'
 import { userReducer, userState } from '../../reducer/userReducer'
 import classes from './HeaderUpdate.module.css'
 
 const HeaderUpdate = ({ setter }) => {
     const apiV1 = process.env.REACT_APP_API_V1
+    const [stateUser] = useReducer(userReducer, userState)
+    const { stateAuth } = useContext(Auth)
+
+    // const [headerData, setHeaderData] = useState([])
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const response = await fetch(`${apiV1}/ep/doctor-ep-header/${stateUser.info.id}`, {
+    //             method: 'GET',
+    //             headers: {
+    //                 Accept: 'application/json',
+    //                 'Content-Type': 'application/json',
+    //             },
+    //         })
+
+    //         const data = await response.json()
+    //         if (response.ok) {
+    //             setHeaderData(data)
+    //         }
+    //     }
+    //     try {
+    //         fetchData()
+    //     } catch {
+    //         setHeaderData([])
+    //     }
+    // }, [apiV1, stateUser.info.id])
+
+    // let leftHeader = headerData?.filter((v) => v.header_side === 'left')
+    // let rightHeader = headerData?.filter((v) => v.header_side === 'right')
 
     const [side, setSide] = useState('left')
     const [heading, setHeading] = useState('')
     const [body, setBody] = useState('')
 
-    const [stateUser] = useReducer(userReducer, userState)
-    const { stateAuth } = useContext(Auth)
+    // useEffect(() => {
+    //     if (side === 'left') {
+    //         setHeading(leftHeader[0]?.heading)
+    //         setBody(leftHeader[0]?.body)
+    //     } else if (side === 'right') {
+    //         setHeading(rightHeader[0]?.heading)
+    //         setBody(rightHeader[0]?.body)
+    //     } else {
+    //         setHeading('')
+    //         setBody('')
+    //     }
+    // }, [side, leftHeader, rightHeader])
 
     const submit = async (e) => {
         e.preventDefault()
@@ -50,13 +88,7 @@ const HeaderUpdate = ({ setter }) => {
                         <option value="right">Right Side</option>
                     </select>
                     <input type="text" value={heading} onChange={(e) => setHeading(e.target.value)} />
-                    <textarea
-                        name=""
-                        id=""
-                        cols="30"
-                        rows="7"
-                        onChange={(e) => setBody(e.target.value)}
-                        value={body}></textarea>
+                    <textarea cols="30" rows="7" value={body} onChange={(e) => setBody(e.target.value)}></textarea>
                     <button onClick={submit}>Update</button>
                 </div>
             </div>
